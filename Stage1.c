fa86d00a53841643d88cde89b9718b911510b35b
Ananya Jajoo <ajajoo1@uci.edu>
2023-10-21 19:32:05 -0700

    signal handling working 2nd time also

--- Stage1.c
+++ Stage1.c
@@ -4,15 +4,17 @@
 #include <string.h>
 #include <unistd.h>
 
+int pid = -1;
+
 void prompt() { printf("\nprompt >"); }
 
 void sigchldHandler(int signo) {
   int status;
   pid_t child_pid;
-  while ((child_pid = waitpid(-1, &status, WNOHANG)) > 0) {
+  /*while ((child_pid = waitpid(-1, &status, WNOHANG)) > 0) {
     // Child process terminated, handle as needed
     printf("Child process with PID %d has terminated\n", child_pid);
-  }
+  }*/
 }
 
 void quitProg() {
@@ -23,60 +25,75 @@ void quitProg() {
     printf("Trying to exit child process");
     exit(0);
     // kill(pid, SIGINT);
+  }
+  if (pid == 0) {
+    printf("%d\n", pid);
+    printf("Trying to kill child process");
+    kill(pid, SIGINT);
   }*/
+
+  if (pid == 0) {
+    printf("%d\n", pid);
+    printf("Trying to exit child process\n");
+    kill(getpid(), SIGINT);
+  }
+
   // exit(0);
 }
 
-void runninginforeground(char command[],char args[]) {
-  int pid = fork();
+void runninginforeground(char command[], char args[]) {
+  pid = fork();
+  // printf("%d\n", pid);
   if (pid == 0) {
+    signal(SIGINT, SIG_DFL);
+    // signal(SIGINT, quitProg);
     sleep(3);
     char direc[1024];
     getcwd(direc, sizeof(direc));
     strcat(direc, "/");
     strcat(direc, command);
-    //system(direc);
-    if(execv(direc,args)<0){
+    // system(direc);
+    if (execv(direc, args) < 0) {
       printf("Program not found");
       exit(0);
     }
-    //printf("\n");
-    exit(0);
+    // printf("\n");
+    // exit(0);
   } else {
     signal(SIGINT, quitProg);
     wait(NULL);
   }
 }
 
-void runninginbackground(char command[],char args[]){
+void runninginbackground(char command[], char args[]) {
   int pid = fork();
   if (pid == 0) {
-    sleep(25);
+    sleep(5);
     char direc[1024];
     getcwd(direc, sizeof(direc));
     strcat(direc, "/");
     strcat(direc, command);
-    //system(direc);
-    char* const argv[]={direc,NULL};
-    if(execv(direc,argv)<0){
+    // system(direc);
+    char* const argv[] = {direc, NULL};
+    if (execv(direc, argv) < 0) {
       printf("Program not found");
       exit(0);
     }
-    //printf("\n");
+    // printf("\n");
   } else {
     printf("Running %s in the background with PID: %d\n", command, pid);
   }
-
 }
 int main() {
   signal(SIGCHLD, sigchldHandler);
+
   while (1) {
     char inp[1024];
     char cwd[1024];
     int command_count;
     prompt();
     fgets(inp, 128, stdin);
-    char command[128]="", args[128]="";
+    char command[128] = "", args[128] = "";
     command_count = sscanf(inp, "%s %s", command, args);
     // printf("%s %s\n", command, args);
     if (strcmp(command, "pwd") == 0) {
@@ -96,19 +113,14 @@ int main() {
 
     else if (strcmp(command, "quit") == 0) {
       break;
-    } 
-    else if(strcmp(command,"")==0){
+    } else if (strcmp(command, "") == 0) {
       printf("Empty Command");
+    } else {
+      if (strcmp(args, "") == 0) {
+        runninginforeground(command, args);
+      } else {
+        runninginbackground(command, args);
       }
-      else {
-        if(strcmp(args,"")==0){
-
-        
-      runninginforeground(command,args);
-        }
-        else{
-          runninginbackground(command,args);
-        }
       /*int pid = fork();
       if (pid == 0) {
         runningfile(command);
