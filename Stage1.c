71ce27ff111041d66afb1d1b2262e3208ac40cf3
Ananya Jajoo <ajajoo1@uci.edu>
2023-10-23 19:51:48 -0700

    90% done - Job Struct Left

--- Stage1.c
+++ Stage1.c
@@ -1,4 +1,5 @@
 #include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,7 +20,7 @@ int pid = -1;
 int job_id = 0;
 int noOfJobs = 0;
 char command[128] = "";
-Job jobs[30];
+Job jobs[5];
 
 void prompt() { printf("prompt >"); }
 
@@ -29,6 +30,18 @@ void sigchldHandler(int signo) {
   while ((child_pid = waitpid(-1, &status, WNOHANG)) > 0) {
     // Child process terminated, handle as needed
     printf("Child process with PID %d has terminated\n", child_pid);
+
+    for (int i = 0; i < noOfJobs; i++) {
+      if (jobs[i].pid == child_pid) {
+        // Remove the job by shifting the remaining jobs
+        for (int j = i; j < noOfJobs - 1; j++) {
+          jobs[j] = jobs[j + 1];
+        }
+        noOfJobs--;
+        printf("Removed job with PID %d from the Job structure\n", child_pid);
+        break;
+      }
+    }
   }
 }
 
@@ -50,47 +63,50 @@ void quitProg() {
   }
 }
 
-void stopForegroundJob() {
-  // printf("Detected with pid %d", pid);
-  jobs[noOfJobs] = create_job(pid, 3, command);
-  // printf("job id %d", job_id);
-  noOfJobs += 1;
-  // kill(pid, SIGTSTP);
-  if (pid != 0) {
-    kill(pid, SIGTSTP);
-  } else {
-    // printf("I am the child process");
+/*void stopForegroundJob() {
+  if (pid == 0) {
+    // printf("Coming the the kill with pid %d ", pid);
     // fflush(stdout);
-    exit(0);
+    pid_t my_pid = getpid();
+    printf("Coming to the kill with pid %d\n", my_pid);
+    fflush(stdout);
+    kill(pid, SIGTSTP);
+    jobs[noOfJobs] = create_job(my_pid, 3, command);
+    noOfJobs += 1;
+  } else {
+    printf("Coming the the exit with pid %d ", pid);
+    fflush(stdout);
+    // exit(0);
+  }
+  if (pid == 0) {
+    // Send SIGTSTP to the child process (foreground job)
+    kill(0, SIGTSTP);
+  }
+}*/
+
+void stopForegroundJob() {
+  if (pid == 0) {
+    return;  // Do nothing for the child process
   }
 
-  /*if (pid != 0) {
-    kill(pid, SIGTSTP);
+  // For the parent process
+  if (kill(pid, SIGTSTP) == 0) {
+    // Successfully sent SIGTSTP to the child
     jobs[noOfJobs] = create_job(pid, 3, command);
-    // printf("job id %d", job_id);
-    noOfJobs += 1;
-    for (int i = 0; i < noOfJobs; i++) {
-      if (jobs[i].pid == pid) {
-        jobs[i].state = STOPPED;
-        break;
-      }
-    }
-  }*/
-  // exit(0);
+    noOfJobs++;
+  } else {
+    perror("kill");
+  }
 }
 
 void runninginforeground(char command[], char args[]) {
   pid = fork();
-  // printf("pid = %d", pid);
-  // jobs[noOfJobs] = create_job(pid, 1, command);
-  // printf("job id %d", job_id);
-  // noOfJobs += 1;
-  // printf("%d\n", pid);
   if (pid == 0) {
+    // pid = getpid();
     signal(SIGINT, SIG_DFL);
-    signal(SIGTSTP, stopForegroundJob);
+    signal(SIGTSTP, SIG_DFL);
     //  signal(SIGINT, quitProg);
-    sleep(3);
+    sleep(30);
     char direc[1024];
     getcwd(direc, sizeof(direc));
     strcat(direc, "/");
@@ -103,7 +119,9 @@ void runninginforeground(char command[], char args[]) {
   } else {
     signal(SIGINT, quitProg);
     signal(SIGTSTP, stopForegroundJob);
-    wait(NULL);
+    int status;
+    waitpid(pid, &status, WUNTRACED);
+    // wait(NULL);
   }
 }
 
@@ -114,8 +132,6 @@ void runninginbackground(char command[], char args[]) {
   strcat(amper, " &");
   jobs[noOfJobs] = create_job(pid, 2, amper);
   noOfJobs += 1;
-  // strcat(command, "&");
-
   if (pid == 0) {
     signal(SIGINT, SIG_IGN);
     sleep(5);
@@ -157,6 +173,154 @@ void print_job_list(Job job_list[]) {
 }
 // Function to get the status string based on the state
 
+// Function to bring a job to the foreground
+void bring_to_foreground(char* arg) {
+  if (arg[0] == '%') {
+    int job_id;
+    bool found = 0;
+    if (sscanf(arg, "%%%d", &job_id) == 1) {
+      bool found = 0;
+      for (int i = 0; i < noOfJobs; i++) {
+        if (jobs[i].job_id == job_id) {
+          kill(jobs[i].pid, SIGCONT);
+          jobs[i].state = 1;
+          found = 1;
+          waitpid(jobs[i].pid, NULL, 0);
+        }
+      }
+      if (!found) {
+        printf("Job with ID %d not found\n", job_id);
+      }
+
+    } else {
+      printf("Invalid job reference format\n");
+    }
+  } else {
+    pid_t pid;
+    if (sscanf(arg, "%d", &pid) == 1) {
+      bool found = 0;
+      int pid3 = atoi(arg);
+      printf("%d \n", pid3);
+      fflush(stdout);
+      kill(pid3, SIGCONT);
+      for (int i = 0; i < noOfJobs; i++) {
+        if (jobs[i].pid == pid3) {
+          found = 1;
+          jobs[i].state = 1;
+          waitpid(jobs[i].pid, NULL, 0);
+        }
+      }
+      if (!found) {
+        printf("Job with PID %d not found\n", pid);
+      }
+
+    } else {
+      printf("Invalid job reference format\n");
+    }
+  }
+}
+
+void bring_to_background(const char* arg) {
+  int job_ref;
+  pid_t pid;
+
+  if (arg[0] == '%') {
+    if (sscanf(arg, "%%%d", &job_ref) == 1) {
+      // Handle JID case
+      for (int i = 0; i < noOfJobs; i++) {
+        if (jobs[i].job_id == job_ref) {
+          if (jobs[i].state == STOPPED) {
+            // If the job is in the STOPPED state, send a SIGCONT signal to
+            // resume it
+            if (kill(jobs[i].pid, SIGCONT) == 0) {
+              jobs[i].state = BACKGROUND_RUNNING;
+              printf("Resumed job %d to Background/Running state\n", job_ref);
+            } else {
+              perror("kill");
+            }
+          } else if (jobs[i].state == BACKGROUND_RUNNING) {
+            printf("Job %d is already in the Background/Running state\n",
+                   job_ref);
+          } else {
+            printf("Job %d is in an invalid state\n", job_ref);
+          }
+          return;
+        }
+      }
+      printf("Job with ID %d not found\n", job_ref);
+    } else {
+      printf("Invalid job reference format\n");
+    }
+  } else if (sscanf(arg, "%d", &pid) == 1) {
+    // Handle PID case
+    for (int i = 0; i < noOfJobs; i++) {
+      if (jobs[i].pid == pid) {
+        if (jobs[i].state == STOPPED) {
+          // If the job is in the STOPPED state, send a SIGCONT signal to resume
+          // it
+          if (kill(jobs[i].pid, SIGCONT) == 0) {
+            jobs[i].state = BACKGROUND_RUNNING;
+            printf("Resumed job with PID %d to Background/Running state\n",
+                   pid);
+          } else {
+            perror("kill");
+          }
+        } else if (jobs[i].state == BACKGROUND_RUNNING) {
+          printf("Job with PID %d is already in the Background/Running state\n",
+                 pid);
+        } else {
+          printf("Job with PID %d is in an invalid state\n", pid);
+        }
+        return;
+      }
+    }
+    printf("Job with PID %d not found\n", pid);
+  } else {
+    printf("Invalid job reference format\n");
+  }
+}
+
+void terminateJob(const char* arg) {
+  int job_ref;
+  pid_t pid;
+  if (arg[0] == '%') {
+    if (sscanf(arg, "%%%d", &job_ref) == 1) {
+      // Handle JID case
+      for (int i = 0; i < noOfJobs; i++) {
+        if (jobs[i].job_id == job_ref) {
+          pid = jobs[i].pid;
+          if (kill(pid, SIGTERM) == 0) {
+            // Successfully sent SIGINT to the job
+            printf("Terminated job %d\n", job_ref);
+          } else {
+            perror("kill");
+          }
+          return;
+        }
+      }
+      printf("Job with ID %d not found\n", job_ref);
+    } else {
+      printf("Invalid job reference format\n");
+    }
+  } else if (sscanf(arg, "%d", &pid) == 1) {
+    // Handle PID case
+    for (int i = 0; i < noOfJobs; i++) {
+      if (jobs[i].pid == pid) {
+        if (kill(pid, SIGTERM) == 0) {
+          // Successfully sent SIGINT to the job
+          printf("Terminated job with PID %d\n", pid);
+        } else {
+          perror("kill");
+        }
+        return;
+      }
+    }
+    printf("Job with PID %d not found\n", pid);
+  } else {
+    printf("Invalid job reference format\n");
+  }
+}
+
 int main() {
   signal(SIGCHLD, sigchldHandler);
   while (1) {
@@ -189,26 +353,21 @@ int main() {
 
     else if (strcmp(command, "quit") == 0) {
       break;
+    } else if (strcmp(command, "fg") == 0) {
+      bring_to_foreground(args);
+    } else if (strcmp(command, "bg") == 0) {
+      // Handle "bg" command
+      bring_to_background(args);
     } else if (strcmp(command, "") == 0) {
       printf("Empty Command");
+    } else if (strcmp(command, "kill") == 0) {
+      terminateJob(args);
     } else {
       if (strcmp(args, "") == 0) {
         runninginforeground(command, args);
       } else {
         runninginbackground(command, args);
       }
-      /*int pid = fork();
-      if (pid == 0) {
-        runningfile(command);
-        break;
-      } else {
-        wait(NULL);
-        exit()
-      }*/
     }
-
-    /*else {
-      printf("Unknown command\n");
-    }*/
   }
 }
